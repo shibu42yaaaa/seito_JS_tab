98de36e0a3c8456d7df017ca8a9c9a498a2cdbe7
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-01-04 16:56:33 +0900

    finish

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 (()=>{
+
   const $doc = document;
   const $tab = $doc.getElementById('js-tab');
   const $nav = $tab.querySelectorAll('[data-nav]');
   const $content = $tab.querySelectorAll('[data-content]');
+  const ACTIVE_CLASS = 'is-active';
+  const navLen = $nav.length;
 
   //初期化
   const init = () => {
@@ -14,17 +17,34 @@
   const handleClick = (e) => {
     e.preventDefault();
 
-    console.log('Clicked!!');
-  };
+    // クリックされたnavとそのdataを取得
+    const $this = e.target;
+    const targetVal = $this.dataset.nav;
 
+    //対象外のnav, content全て一旦リセットする
+    let index = 0;
+    while(index < navLen){
+        $content[index].style.display = 'none';
+        $nav[index].classList.remove(ACTIVE_CLASS);
+        index++;
+    }
+
+    // console.log('targetVal', targetVal);
+
+    //対象のコンテンツをアクティブ化する
+    $tab.querySelectorAll('[data-content="'+ targetVal +'"]')[0].style.display = 'block';
+    $nav[targetVal].classList.add(ACTIVE_CLASS);
+
+    console.log('$nav[targetVal].classList', $nav[targetVal].classList);
+
+  };
+  
+  //全nav要素に対して関数を適用
   let index = 0;
-  while(index < $nav.length){
+  while(index < navLen){
     $nav[index].addEventListener('click', (e) => handleClick(e));
     index++;
   }
 
-  
-
-// console.log('$content', $content)
-
 })();
+
